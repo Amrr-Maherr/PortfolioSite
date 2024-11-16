4899daf2b5b3aa86ea41898dc61f3252d6ceed38
Amr maher <amrr.maherr24@gmail.com>
2024-11-16 15:47:23 +0200

    Add explanatory comments to the Contact component code
    Added detailed comments to the Contact component to explain:
    The structure of the component.
    Purpose of Bootstrap classes used.
    Accessibility attributes in the input and button elements.
    Export functionality of the component.

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,22 +1,41 @@
+// Defining the Contact component
 function Contact() {
     return (
         <>
-           <div className="container Contact text-center p-4 bg-dark w-75">
-    <div className="row">
-        <div className="col-lg-6 col-12">
-            <h3>Get In Touch</h3>
-            <p>If you'd like to discuss a project, ask a question, or collaborate, feel free to reach out to me!</p>
-        </div>
-        <div className="col-lg-6 col-12 p-4">
-            <div className="input-group mb-3">
-                <input type="text" className="form-control" placeholder="Your message" aria-label="Your message" aria-describedby="button-addon2" />
-                <button className="btn btn-outline-secondary" type="button" id="button-addon2">Send</button>
+            {/* A container for the contact section */}
+            <div className="container Contact text-center p-4 bg-dark w-75">
+                <div className="row">
+                    {/* Left column: Contact message */}
+                    <div className="col-lg-6 col-12">
+                        <h3>Get In Touch</h3>
+                        <p>If you'd like to discuss a project, ask a question, or collaborate, feel free to reach out to me!</p>
+                    </div>
+                    {/* Right column: Input field and send button */}
+                    <div className="col-lg-6 col-12 p-4">
+                        <div className="input-group mb-3">
+                            {/* Input field for the user's message */}
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Your message"
+                                aria-label="Your message"
+                                aria-describedby="button-addon2"
+                            />
+                            {/* Send button */}
+                            <button
+                                className="btn btn-outline-secondary"
+                                type="button"
+                                id="button-addon2"
+                            >
+                                Send
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-</div>
-
         </>
-    )
+    );
 }
-export default Contact
+
+// Exporting the Contact component to make it available for use in other files
+export default Contact;
